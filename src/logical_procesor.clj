3ae989913cc670a4ff35d4d3f596410b3ec5499b
Your Name <you@example.com>
2017-08-30 22:48:00 -0300

    initial

--- src/logical_procesor.clj
+++ src/logical_procesor.clj
@@ -1,6 +1,7 @@
 (ns logical_procesor)
 
-(defn only-greater-than-five
-  "Given a list, filter for some condition"
-  [list]
-  (filter ( fn [x] (> x 5)) list))
+(defn get-facts-map
+  "Generate the facts map"
+  []
+  {:a (list '1 '2 '3)}
+  )
--- test/logical_procesor_test.clj
+++ test/logical_procesor_test.clj
@@ -2,22 +2,13 @@
     (:require [clojure.test :refer :all]
               [logical_procesor :refer :all]))
 
-(deftest check-the-mock-facts-list
+(deftest check-the-type-of-facts-list
   (testing "Tests the mock facts list"
-           (is (= (facts-mock-list) '()))
+           (is true (instance? (type get-facts-map) java.util.Map))
+  ))
 
-(deftest only-greater-than-five-test-empty-list
-  (testing "Tests to filter an empty list."
-           (is (= (only-greater-than-five '()) '()))))
-
-(deftest only-greater-than-five-test-less-than-five-elements
-  (testing "Tests to filter a list with elements that are less than five."
-           (is (= (only-greater-than-five '(1 2 3 4)) '()))))
-
-(deftest only-greater-than-five-test-only-element-five
-  (testing "Tests to filter a list with only one element which is the number five."
-           (is (= (only-greater-than-five '(5)) '()))))
-
-(deftest only-greater-than-five-test-greater-than-five-elements
-  (testing "Tests to filter a list with elements that are less than five."
-           (is (= (only-greater-than-five '(6 7)) '(6 7)))))
+(deftest check-the-type-of-facts-list-item
+  (testing "Tests the mock facts list"
+           (def lists ((get-facts-map) :a))
+           (is true (instance? (type lists) (type (list 'a 'b))))
+   ))
